2f7bde2c424832cb036f9db416ab23487261939d
dodoyoo <dong15903@daum.net>
2025-07-31 11:38:51 +0900

    feat: Post openAI

--- src/domain/comment/commentRoute.ts
+++ src/domain/comment/commentRoute.ts
@@ -6,7 +6,7 @@ const router = Router();
 const commentRepository = new CommentRepository();
 const commentController = new CommentController(commentRepository);
 
-router.post('/api/comment/:userId', (req: Request, res: Response) =>
+router.post('/api/question/:userId', (req: Request, res: Response) =>
   commentController.createComments(req, res)
 );
 
