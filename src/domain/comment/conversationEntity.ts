12661c266b73151eda3c7c0eb20c772b4e591604
dodoyoo <dong15903@daum.net>
2025-08-03 13:58:09 +0900

    fix: entity

--- src/domain/comment/conversationEntity.ts
+++ src/domain/comment/conversationEntity.ts
@@ -18,6 +18,10 @@ export class Conversation {
   @Column({ type: 'number', nullable: false })
   user_id!: number;
 
-  @CreateDateColumn({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  @CreateDateColumn({
+    type: 'timestamp',
+    nullable: false,
+    default: () => 'CURRENT_TIMESTAMP',
+  })
   created_at!: Date;
 }
